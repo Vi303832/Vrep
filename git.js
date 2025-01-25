817656728a71ae6d87ae12766e92603b71ceb0e8
Akif <mehmettanyeriakif3@hotmail.com>
2025-01-25 15:53:11 +0300

    Added functions to texts

--- git.js
+++ git.js
@@ -9,9 +9,18 @@ function handleclick() {
     let div = document.createElement("div");
     let bt = document.createElement("button");
     let bt2 = document.createElement("button");
+    let newip = document.createElement("input");
+    let newbut = document.createElement("button");
+
+    newip.type = "text";
+    newbut.textContent = "Done"
 
     bt.textContent = "X";
     bt2.textContent = "adjust";
+
+
+
+
     div.textContent = imp.value
     div.classList.add("out")
     span.classList.add("texst")
@@ -23,6 +32,38 @@ function handleclick() {
     content.appendChild(span)
 
 
+    bt.addEventListener("click", () => { content.removeChild(span) })
+
+    bt2.addEventListener("click", () => {
+        span.removeChild(div)
+        span.removeChild(bt)
+        span.removeChild(bt2)
+        span.appendChild(newip);
+        span.appendChild(newbut);
+    },
+        newbut.addEventListener("click", () => {
+
+            div.textContent = newip.value
+
+            span.removeChild(newip)
+            span.removeChild(newbut)
+
+            span.appendChild(div)
+            span.appendChild(bt)
+            span.appendChild(bt2)
+
+
+
+
+        })
+
+
+
+
+
+
+
+    )
 
 
 
